233424398a01019db452f7b3637226e1c9e02f73
jessicarandall <jess1998mat@gmail.com>
2023-05-07 19:16:25 +0200

    initial commit

---
+++ app/src/main/java/com/starchild13/greetingcard/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.starchild13.greetingcard
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import com.starchild13.greetingcard.ui.theme.GreetingCardTheme
+import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.layout.padding
+import androidx.compose.ui.unit.sp
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            GreetingCardTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Starchild")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Surface(color = Color.Blue) {
+        Text(text = "Hi my name is $name!", modifier = Modifier.padding(24.dp), fontSize = 36.sp)
+
+    }
+}
+
+@Preview(showBackground = false)
+@Composable
+fun DefaultPreview() {
+    GreetingCardTheme {
+        Greeting("Starchild")
+    }
+}
